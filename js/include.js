faab9a8a71cff8babff6f9cb29e80b3f57be8591
codecortex101-tech <codecortex101@gmail.com>
2025-12-18 14:54:57 +0500

    Delete js directory

--- js/include.js
+++
@@ -1,7 +0,0 @@
-fetch('./header.html')
-.then(r => r.text())
-.then(d => document.getElementById('site-header').innerHTML = d);
-
-fetch('./footer.html')
-.then(r => r.text())
-.then(d => document.getElementById('site-footer').innerHTML = d);
